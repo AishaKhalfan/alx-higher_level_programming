46d442181f7249ab817c9d9de45b1836b4594f26
AishaKhalfan <aishakhalfan45@gmail.com>
2022-12-01 21:04:19 +0000

    a program that imports the function def add(a, b): from the file add_0.py and prints the result of the addition 1 + 2 = 3

---
+++ 0x01-python-if_else_loops_functions/13-insert_number.c
@@ -0,0 +1,15 @@
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include "lists.h"
+
+/**
+* insert_node -inserts a number into a sorted singly linked list
+* @head: pointer to head of list
+* Return: address of new node
+* or NULL if failed
+*/
+listint_t *insert_node(listint_t **head, int number)
+{
+
+}
