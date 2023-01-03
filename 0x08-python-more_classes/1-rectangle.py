f894463309a3f41f6f4f1bb9f612d1b5df8d32ee
AishaKhalfan <aishakhalfan45@gmail.com>
2023-01-03 20:31:23 +0000

    a class Rectangle that defines a rectangle by: (based on 0-rectangle.py)

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -29,7 +29,7 @@ class Rectangle():
             if value >= 0:
                 self.__height = value
             else:
-                raise ValueError("height must be >= 0:")
+                raise ValueError("height must be >= 0")
         else:
             raise TypeError("height must be an integer")
 
@@ -45,6 +45,6 @@ class Rectangle():
             if value >= 0:
                 self.__width = value
             else:
-                raise ValueError("width must be >= 0:")
+                raise ValueError("width must be >= 0")
         else:
             raise TypeError("width must be an integer")
