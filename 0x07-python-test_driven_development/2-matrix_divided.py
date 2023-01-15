e3fb8f0020122baeb934b167098874c5c8a4b83a
AishaKhalfan <aishakhalfan45@gmail.com>
2023-01-15 16:37:07 +0000

    a function that divides all elements of a matrix.

--- 0x07-python-test_driven_development/2-matrix_divided.py
+++ 0x07-python-test_driven_development/2-matrix_divided.py
@@ -1,39 +1,39 @@
 #!/usr/bin/python3
 """
-This is the 2-matrix_divided module.
-The 2-matrix_divided module supplies one function,matrix_divided()
+This is the matrix_divided module.
+This module supplies one function, matrix_divided().
 """
 
 
 def matrix_divided(matrix, div):
     """
-    Return the division of matrix and div.
+    Return a new matrix where each element has been divided by div.
     Args:
-        matrix (list(list)): the first value
-        div (int or float): the second value
+        matrix (list): list of lists of integers or floats.
+        div (int, float): the divider, >= 0.
     """
-    mtrx_type_E = 'matrix must be a matrix (list of lists) of integers/floats'
-    mtrx_lenError = 'Each row of the mmatrix must have the same size'
-    div_typError = 'div must be a number'
-    div_zeroError = 'division by zero'
+    mtrx_type_e = 'matrix must be a matrix (list of lists) of integers/floats'
+    mtrx_len_e = 'Each row of the matrix must have the same size'
+    div_type_e = 'div must be a number'
+    div_zero_e = 'division by zero'
 
     row_len = 0
     if type(matrix) is not list:
-        raise TypeError(mtrx_type_E)
+        raise TypeError(mtrx_type_e)
 
     for row in matrix:
         if type(row) is not list:
-            raise TypeError(mtrx_type_E)
+            raise TypeError(mtrx_type_e)
         for i in row:
             if type(i) not in [int, float]:
-                raise TypeError(mtrx_type_E)
+                raise TypeError(mtrx_type_e)
         if len(row) is not row_len and row_len is not 0:
-            raise TypeError(mtrx_lenError)
+            raise TypeError(mtrx_len_e)
         row_len = len(row)
 
     if type(div) not in [int, float]:
-        raise TypeError(div_typError)
+        raise TypeError(div_type_e)
     if div is 0:
-        raise ZeroDivisionError(div_zeroError)
+        raise ZeroDivisionError(div_zero_e)
 
     return [[round(nb / div, 2) for nb in row] for row in matrix]
