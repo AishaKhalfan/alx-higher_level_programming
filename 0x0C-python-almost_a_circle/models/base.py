0dafa0d0bb5ed33df7ffad412ea5b01eaa7bf66e
AishaKhalfan <aishakhalfan45@gmail.com>
2023-01-22 11:01:23 +0000

    Update the class Base by adding the static method def to_json_string(list_dictionaries): that returns the JSON string representation of list_dictionaries

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -28,4 +28,6 @@ class Base:
 
     def to_json_string(list_dictionaries):
         """Returns the JSON representation of a string object."""
+        if list_dictionaries is None or len(list_dictionaries) < 0:
+            return "[]"
         return json.dumps(list_dictionaries)
