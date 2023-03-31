33960352d25781ee4c1d792c62accf603bde090c
AishaK <aishakhalfan45@gmail.com>
2023-03-31 10:12:48 +0000

    a Python script that fetches https://alx-intranet.hbtn.io/status

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-# a Python script that fetches https://alx-intranet.hbtn.io/status
+"""a Python script that fetches https://alx-intranet.hbtn.io/status"""
 
 import  urllib.request
 
@@ -12,5 +12,3 @@ if __name__ == "__main__":
         print("\t- type: {}".format(type(body)))
         print("\t- content: {}".format(body))
         print("\t- utf8 content: {}".format(body.decode("utf-8")))
-
-
