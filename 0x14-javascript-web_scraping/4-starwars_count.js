f32d84e45fbf9ae80cb5553df9fb56763466eab1
AishaK <aishakhalfan45@gmail.com>
2023-04-26 20:40:44 +0000

    star wars

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -1,20 +1,24 @@
 #!/usr/bin/node
-/* a script that prints the title of a Star Wars movie where
-the episode number matches a given integer. */
 
 const request = require('request');
 const url = process.argv[2];
-request(url, (err, res, body) => {
-  if (err) console.error(err);
-  const data = JSON.parse(body);
-  let count = 0;
-  for (let i = 0; i < data.results.length; i++) {
-    const chars = data.results[i].characters;
-    for(let k = 0; k < chars.length; k++) {	  
-      if (chars.includes("18")){
-      count ++;
-    }
-   }
+
+request(url, (error, response, body) => {
+  if (error) {
+    console.error(error);
+    return;
   }
+
+  const films = JSON.parse(body).results;
+  let count = 0;
+
+  for (let i = 0; i < films.length; i++) {
+    const characters = films[i].characters;
+    if (characters.includes('https://swapi-api.alx-tools.com/api/people/18/')) {
+      count++;
+    }
+  }
+
   console.log(count);
 });
+
