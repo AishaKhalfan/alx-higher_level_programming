ec2e591545b4f1d55c125e4566b9e5ed6ae054ca
AishaK <aishakhalfan45@gmail.com>
2023-04-26 20:49:33 +0000

    star wars

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -1,24 +1,29 @@
 #!/usr/bin/node
 
+/*
+This script retrieves JSON data from an external API and counts the number of movies in which a character with ID ending in "/18/" appears.
+*/
+
+// Import the 'request' library, which allows us to make HTTP requests in Node.js
 const request = require('request');
-const url = process.argv[2];
 
-request(url, (error, response, body) => {
-  if (error) {
-    console.error(error);
-    return;
+// Send an HTTP GET request to the URL passed in as the third argument to the script
+request(process.argv[2], function (error, response, body) {
+  // Check if there was no error in the request
+  if (!error) {
+    // Parse the response body as JSON
+    const results = JSON.parse(body).results;
+
+    // Use the 'reduce' method to count the number of movies in which a character with ID ending in '/18/' appears
+    const numMovies = results.reduce((count, movie) => {
+      // Check if there is any character in the 'movie' array whose ID ends with '/18/'
+      return movie.characters.find((character) => character.endsWith('/18/'))
+        ? count + 1 // If there is, increment the 'count'
+        : count; // Otherwise, return the current 'count'
+    }, 0);
+
+    // Print the number of movies to the console
+    console.log(numMovies);
   }
-
-  const films = JSON.parse(body).results;
-  let count = 0;
-
-  for (let i = 0; i < films.length; i++) {
-    const characters = films[i].characters;
-    if (characters.includes('https://swapi-api.alx-tools.com/api/people/18/')) {
-      count++;
-    }
-  }
-
-  console.log(count);
 });
 
