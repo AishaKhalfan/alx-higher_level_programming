36555cd7aa8af86c5108f8a3dbd30cfecae4f364
AishaK <aishakhalfan45@gmail.com>
2023-05-03 20:34:02 +0000

    JavaScript - Web jQuery

--- 0x15-javascript-web_jquery/0-script.js
+++ 0x15-javascript-web_jquery/0-script.js
@@ -1,5 +1,2 @@
-// Get the header element
-const header = document.querySelector("header");
-
-// Set the text color to red
-header.style.color = "#FF0000";
+'use strict';
+document.querySelector('header').style.color = '#FF0000';
